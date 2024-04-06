18de5079676d2cfaa5ba98b6ae2ff96c246551eb
ValeriaLukovich <valeriialukovich@Valeriias-MacBook-Air.local>
2024-04-06 14:02:17 +0200

    2

--- manage.py
+++ manage.py
@@ -7,7 +7,7 @@ import sys
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'task_manager.settings')
-    django.core.management import execute_from_command_line
+    from django.core.management import execute_from_command_line
     execute_from_command_line(sys.argv)
 
 
