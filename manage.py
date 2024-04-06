c8496fed2b64afa44105001df663cf249f75ca78
ValeriaLukovich <valeriialukovich@Valeriias-MacBook-Air.local>
2024-04-06 08:05:46 +0200

    2

--- manage.py
+++ manage.py
@@ -2,8 +2,13 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-import django
 
+
+from dotenv import load_dotenv
+
+if __name__ == '__main__':
+    load_dotenv()
+    
 
 def main():
     """Run administrative tasks."""
@@ -17,7 +22,3 @@ def main():
             "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
-
-
-if __name__ == '__main__':
-    main()
