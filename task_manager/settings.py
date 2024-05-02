4f542d88e31f30f58e9de499bf743aad725fba4d
ValeriaLukovich <valeriialukovich@Valeriias-MacBook-Air.local>
2024-05-02 20:58:48 +0200

    add codeclimate

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '#a-%u(0yr@^n_4_8jih^_ez*x=t99ropgu^q7mvy&vt!!qj4v$'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', False)
