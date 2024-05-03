34bd4be73c9e6cf2ab8ea8c49ef8b5b66f4a5ecc
ValeriaLukovich <valeriialukovich@Valeriias-MacBook-Air.local>
2024-05-03 14:19:25 +0200

    add secret_key

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = 'l0ib*0p^7^d%i*cqfz)2@hnqp4k#0yl*)m9jo(tit8ujv7l)7a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', False) == 'True'
