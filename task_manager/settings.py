2e71f836d41129be34ffa6d675522bcfd8e3862f
ValeriaLukovich <valeriialukovich@Valeriias-MacBook-Air.local>
2024-05-09 10:51:33 +0200

    internationalisation

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -125,10 +125,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'ru-Ru'
 
-#LANGUAGES = (
-#    ('en-US', 'English'),
-#    ('ru-RU', 'Russian'),
-#)
+
 
 LOCALE_PATHS = (os.path.join(BASE_DIR, 'locale'), )
 
